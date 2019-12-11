ca290a87346e43b421b7ec70629a2df073998101
Sandeep <skm00773@gmail.com>
2019-12-11 14:02:41 +0530

    comit c

---
+++ Project.Managment/App_Start/BundleConfig.cs
@@ -0,0 +1,64 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Project.Managment
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //This ScriptBundle will use for login layout only
+            #region LOGIN SCRIPTS.* / StyleBundle.*
+            bundles.Add(new StyleBundle("~/public/css").Include(
+                      "~/Asset/css/font-awesome.css",
+                      "~/Asset/css/style.css",
+                      "~/Asset/css/pages/signin.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/public").Include(
+                        "~/Asset/js/js/signin.js"));
+            #endregion
+
+            #region DASHBOARD
+            bundles.Add(new StyleBundle("~/dashboard/css").Include(
+                      //"~/Asset/css/bootstrap.min.css",
+                      "~/Asset/css/bootstrap-responsive.min.css",
+                      "~/Asset/css/font-awesome.css",
+                      "~/Asset/css/style.css",
+                      "~/Asset/css/pages/dashboard.css",
+                      "~/Asset/css/custom.css"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/dashboard/public").Include(
+                      //"~/Asset/js/excanvas.min.js",
+                      //"~/Asset/js/chart.min.js",
+                      //"~/Asset/js/full-calendar/fullcalendar.min.js",
+                      "~/Asset/js/base.js"));
+
+
+
+            #endregion
+
+
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Project.Managment/Asset/js/CustomLogics.js
@@ -0,0 +1,61 @@
+﻿//ON READY INIT REQUIRED FUNTIONS OR ANY....
+
+$(document).ready(function () {
+    //$('#project').DataTable(
+    //    {
+    //        dom: 'Bfrtip',
+    //        buttons: [
+    //            {
+    //                extend: 'csv',
+    //                text: 'Export to Excel  ',
+    //                className: 'btn btn-success fa-file-excel-o',
+    //                //exportOptions: {
+    //                //    modifier: {
+    //                //        page: 'current'
+    //                //    }
+    //                //}
+    //            }
+    //            //'copy', 'csv', 'excel', 'pdf', 'print'
+    //        ]
+    //    }
+    //);
+    $('#project').DataTable();
+
+
+    //$('#project').DataTable({
+    //    language: {
+    //        processing: "Traitement en cours...",
+    //        search: "Rechercher&nbsp;:",
+    //        lengthMenu: "Afficher _MENU_ &eacute;l&eacute;ments",
+    //        info: "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+    //        infoEmpty: "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+    //        infoFiltered: "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+    //        infoPostFix: "",
+    //        loadingRecords: "Chargement en cours...",
+    //        zeroRecords: "Aucun &eacute;l&eacute;ment &agrave; afficher",
+    //        emptyTable: "Aucune donnée disponible dans le tableau",
+    //        paginate: {
+    //            first: "Premier",
+    //            previous: "Pr&eacute;c&eacute;dent",
+    //            next: "Suivant",
+    //            last: "Dernier"
+    //        },
+    //        aria: {
+    //            sortAscending: ": activer pour trier la colonne par ordre croissant",
+    //            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+    //        }
+    //    }
+    //});
+});
+
+var tableToExcel = (function () {
+    var uri = 'data:application/vnd.ms-excel;base64,'
+        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+        , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+        , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+    return function (table, name) {
+        if (!table.nodeType) table = document.getElementById(table)
+        var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+        window.location.href = uri + base64(format(template, ctx))
+    }
+})();
